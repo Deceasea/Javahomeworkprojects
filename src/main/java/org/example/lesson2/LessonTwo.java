c6987df02a6dfaa91eb7221cffe00621d0d4be3e
Decease <alshera13@mail.ru>
2022-11-10 01:02:37 +0300

    more steps to java

---
+++ src/main/java/org/example/lesson2/LessonTwo.java
@@ -0,0 +1,32 @@
+package org.example.lesson2;
+
+public class LessonTwo {
+    public static void main(String[] args) {
+        System.out.println(checkSum( 6,7));
+        checkNumber(8);
+        System.out.println(checkNumberTwo (-7));
+        repeatingString("Shame on you!", 3);
+    }
+    public static boolean checkSum( int a, int b ) {
+        return a + b >= 10 && a + b <= 20;
+
+
+    }
+    public static void checkNumber ( int x) {
+        if (x >= 0) {
+            System.out.println("positive");
+        } else {
+            System.out.println("negative");
+        }
+
+}
+    public static boolean checkNumberTwo (int z) {
+        return z < 0;
+    }
+    public static void repeatingString ( String str, int y ) {
+        for (int i = 0; i < y; i++) {
+            System.out.println(str);
+        }
+    }
+}
+
